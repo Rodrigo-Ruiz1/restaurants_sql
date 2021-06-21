2523bdceaaff9cc548b7b7cecba6ac3de354ff0f
Rodrigo Ruiz <ruiz.rodrigo46@yahoo.com>
2021-06-21 16:43:30 -0400

    initial commit for small sql exercise

---
+++ seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO restaurants
+    (name, distance, stars, category, favorite_dish, does_takeout, last_visit)
+VALUES
+    ('Casa Bariachi', 3.3, 5, 'Mexican', 'Quesadilla', TRUE, '2021-5-1'),
+    ('Kadaydles', 3.8, 4, 'Sandwiches', 'Turkey, Bacon, and Swiss', TRUE, '2021-3-31'),
+    ('Midtown Grill', 3.5, 5, 'American', 'Midtown Burger', TRUE, '2020-12-1');
